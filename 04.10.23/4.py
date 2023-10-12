2d91000e7a39295e9b2ee26a7cda68a16ac7b461
sofiiasliusar <sofiia.r.sliusar@ukd.edu.ua>
2023-10-12 15:10:26 +0300

    completed

---
+++ 04.10.23/4.py
@@ -0,0 +1,24 @@
+
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# *
+
+n = 10  
+
+
+for i in range(n):
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
+
+
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
